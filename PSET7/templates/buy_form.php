a139b10c45adf7c2e17cb4586a3618cd60d7eb07
Andy <andyznyc@users.noreply.github.com>
2015-01-20 19:21:08 -0500

    Create buy_form.php

---
+++ PSET7/templates/buy_form.php
@@ -0,0 +1,16 @@
+<form action="buy.php" method="post">
+    <fieldset>
+        <div class="form-group">
+            <input autofocus class="form-control" name="symbol" placeholder="Symbol" type="text"/>
+        </div>
+        <div class="form-group">
+            <input autofocus class="form-control" name="amount" placeholder="Amount" type="text"/>
+        </div>
+         <div class="form-group">
+            <button type="submit" class="btn btn-default">BUY</button>
+        </div>
+    </fieldset>
+</form>
+<div>
+    <a href="/index.php">Home</a> 
+</div>
