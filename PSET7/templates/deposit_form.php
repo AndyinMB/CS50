0db74094c3ac7d9dc711ad0e1cf275705e9e41cb
Andy <andyznyc@users.noreply.github.com>
2015-01-20 19:21:28 -0500

    Create deposit_form.php

---
+++ PSET7/templates/deposit_form.php
@@ -0,0 +1,13 @@
+<form action="deposit.php" method="post">
+    <fieldset>
+        <div class="form-group">
+            <input autofocus class="form-control" name="deposit" placeholder="Amount" type="text"/>
+        </div>
+         <div class="form-group">
+            <button type="submit" class="btn btn-default">ADD FUNDS</button>
+        </div>
+    </fieldset>
+</form>
+<div>
+    <a href="/index.php">Home</a> 
+</div>
